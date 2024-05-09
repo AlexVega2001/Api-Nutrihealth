ff7b76c032140bf3f7db6d4febd9df872074d429
AlexVega2001 <alexander_vega1111@hotmail.com>
2024-05-09 10:11:04 -0500

    Modificación en la conexión a postgreSQL

--- database/connection.js
+++ database/connection.js
@@ -2,13 +2,23 @@ import "dotenv/config";
 import pkg from "pg";
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  allowExitOnIdle: true,
-});
+const usuarioPG = process.env.PGUSER;
+const hostPG = process.env.PGHOST;
+const passwordPG = process.env.PGPASSWORD;
+const databasePG = process.env.PGDATABASE;
+const portPG = process.env.PGPORT;
+
+// Configuración de la conexión usando variables de entorno
+const poolConfig = {
+  connectionString: `postgresql://${usuarioPG}:${passwordPG}@${hostPG}:${portPG}/${databasePG}`,
+  allowExitOnIdle: true
+};
+
+export const pool = new Pool(poolConfig);
 
 try {
   await pool.query("SELECT NOW()");
   console.log("Connected to Postgres");
 } catch (error) {
-  console.log(error);
+  console.log("Connection error:", error);
 }
